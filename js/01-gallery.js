d097051f1c299620212ac2a4622340558f75f721
Lifuss <arsen.hryh@gmail.com>
2023-08-09 19:06:27 +0300

    task1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// import * as basicLightbox from "basiclightbox";
 
-console.log(galleryItems);
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+};
+
+function renderMarkup(items) {
+  const markup = items.map(createMarkup).join("");
+  refs.galleryList.innerHTML = markup;
+}
+
+function createMarkup({ preview, description, original }) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`;
+}
+
+renderMarkup(galleryItems);
+
+refs.galleryList.addEventListener("click", (e) => {
+  e.preventDefault();
+  basicLightbox
+    .create(`<img src=${e.target.dataset.source}>`)
+    .show((ist) => document.addEventListener("keydown", isEscape));
+});
+function isEscape(event) {
+  if (event.key !== "Escape") return;
+  hideModal();
+  document.removeEventListener("keydown", isEscape);
+}
+
+function hideModal() {
+  document.querySelector(".basicLightbox").remove();
+  // ну не зміг я додуматися як закрити не видаляючи,
+  // бібліотека не дає закрити норм на Escape(так то міг але тоді слухач keydown лишався бо аннонімна функція виходить)
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryList: document.querySelector(".gallery"),
+};
+
+function renderMarkup(items) {
+  const markup = items.map(createMarkup).join("");
+  refs.galleryList.innerHTML = markup;
+}
+
+function createMarkup({ preview, description, original }) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img class="gallery__image" src=${preview} alt='${description}' />
+  </a>`;
+}
+
+renderMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
